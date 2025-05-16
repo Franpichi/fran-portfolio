60f5ecd9e9a8d0c5988d23629e53200d93d57028
Franpichi <franpizzichini@gmail.com>
2025-05-16 13:11:53 +0200

    Fix casing and missing types for deployment

--- src/pages/thankyou.tsx
+++ src/pages/thankyou.tsx
@@ -1,5 +1,5 @@
 import { useTranslation } from 'react-i18next';
-import Layout from '../components/Layout';
+import Layout from '../components/layout';
 
 export default function ThankYou() {
   const { t } = useTranslation();

37a63ad2ccc0ccbe3ebae02133f4a7290510afd5
FrancoPizzichini <68820600+Franpichi@users.noreply.github.com>
2026-03-19 12:42:47 +0100

    Refactor Layout component and add Footer import

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,14 @@
-import Navbar from './navbar';
+import Navbar from "./navbar";
+import Footer from "./footer";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <>
+    <div className="bg-secondary text-white min-h-screen font-sans">
       <Navbar />
-      <main className="min-h-screen px-6 py-16 max-w-6xl mx-auto">{children}</main>
-    </>
+      <main className="px-6 md:px-16 lg:px-32">{children}</main>
+      <Footer />
+    </div>
   );
-}
+};
+
+export default Layout;

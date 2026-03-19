2f5211e490d40d5f2c1c4307a1147c24caa1bdbc
FrancoPizzichini <68820600+Franpichi@users.noreply.github.com>
2026-03-19 13:01:02 +0100

    Refactor i18n.js to use new import syntax

--- src/i18n.js
+++ src/i18n.js
@@ -1,22 +1,18 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import translationEN from './locales/en.json';
-import translationES from './locales/es.json';
-
-const resources = {
-  en: { translation: translationEN },
-  es: { translation: translationES }
-};
+import es from './locales/es.json';
+import en from './locales/en.json';
 
 i18n
-    .use(initReactI18next)
-    .init({
-      resources,
-      lng: 'en',
-      fallbackLng: 'en',
-      interpolation: {
-        escapeValue: false
-      }
-    });
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: { translation: en },
+      es: { translation: es },
+    },
+    lng: 'en',
+    fallbackLng: 'en',
+    interpolation: { escapeValue: false },
+  });
 
 export default i18n;

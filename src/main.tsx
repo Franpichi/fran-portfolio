05c14ee9b9cfde458d0ac1436524c320bccb283a
FrancoPizzichini <68820600+Franpichi@users.noreply.github.com>
2026-03-19 13:00:48 +0100

    Remove BrowserRouter and update i18n import comment

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
-import './i18n'; // i18next config
+import './i18n'; // configuración de i18next
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
